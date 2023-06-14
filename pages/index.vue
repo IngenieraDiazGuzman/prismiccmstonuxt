f7dfe0e0c3f07c6dd344d74ec2f627690de44613
IngenieraDiazGuzman <BugCoppyhacker@gmail.com>
2023-06-13 23:28:34 -0600

    Se agrega el Proyecto

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="lg:pl-24 lg:pr-24 pt-4">
+    <div class="text-center bg-white rounded-xl shadow-md">
+      <h1 class="blog-title">
+        Hello World
+      </h1>
+      <div class="py-8 px-8 mx-auto space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 md:py-4 md:flex md:items-center md:space-y-0 md:space-x-6">
+        <img
+          src="https://tom.imgix.net/1x1_motif.png?ixlib=vue-2.9.0&auto=format%2Ccompress&txt=Place%20Holder&ar=1%3A1&bg=grey&txt-align=middle%2Ccenter&txt-size=75&txt-fit=max&w=380"
+        />
+        <div class="text-center space-y-2">
+          <div class="space-y-0.5">
+          <p class="blog-description inline">This is my blog post.</p> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
